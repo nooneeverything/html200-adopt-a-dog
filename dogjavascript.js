571c774db0ae41838e383b2b358c0ba6dd1aea1e
Noah Tesema <noahtesema@gmail.com>
2023-01-11 22:12:54 -0800

    Lesson 3 Assignment 4 FINAL

---
+++ dogjavascript.js
@@ -0,0 +1,13 @@
+function info(name, breed, cost){
+  alert(name + " - " + breed + " - " + cost);
+}
+
+
+let priceNow = 0;
+let price = '';
+
+function total(price){
+  priceNow = price + priceNow;
+  alert(priceNow);
+}
+ 
